546c6d58131f5097291f15daef961a039f971b65
lodenrogue <phyzix5761@gmail.com>
2024-05-07 15:32:21 -0400

    Added google search, ibuffer, and ignoring elf files in recent files view

--- init.el
+++ init.el
@@ -11,3 +11,15 @@
 ;;(add-to-list 'golden-ratio-extra-commands 'ace-window)
 ;;
 ;;(golden-ratio-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(dired-preview ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
