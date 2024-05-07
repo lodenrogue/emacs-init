fc33dbe82f46eb117aa9c291a7e7678b2e6b3ee2
lodenrogue <phyzix5761@gmail.com>
2024-05-06 23:53:45 -0400

    Clean up

--- init.el
+++ init.el
@@ -11,16 +11,3 @@
 ;;(add-to-list 'golden-ratio-extra-commands 'ace-window)
 ;;
 ;;(golden-ratio-mode 1)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(elfeed sudoku evil)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
