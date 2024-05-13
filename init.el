ae8e73f894dae47dd116390173130d2dd41b8198
lodenrogue <phyzix5761@gmail.com>
2024-05-13 15:36:19 -0400

    Moved installing packages to new file

--- init.el
+++ init.el
@@ -1,22 +1,15 @@
+;; Install Packages ;;
+(load-file "~/.emacs.d/install-packages.el")
+
 (org-babel-load-file
  (expand-file-name "config.org" "~/.emacs.d"))
 
-;;;;;; Golden Ratio (Auto Window Resizing) ;;;;;;
-
-;;(unless (package-installed-p 'golden-ratio)
-;;(package-install 'golden-ratio))
-;;
-;;(require 'golden-ratio)
-;;
-;;(add-to-list 'golden-ratio-extra-commands 'ace-window)
-;;
-;;(golden-ratio-mode 1)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(doom-modeline nerd-icons dired-preview ace-window)))
+ '(package-selected-packages '(ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
