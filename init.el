3aa049d0fe399d4548021cc57533f4eb822c0414
lodenrogue <phyzix5761@gmail.com>
2024-05-22 18:10:49 -0400

    Moved installing packages back to config.org

--- init.el
+++ init.el
@@ -1,5 +1,13 @@
-;; Install Packages ;;
-(load-file "~/.emacs.d/install-packages.el")
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+;; Remove this if behind a firewall
+(package-refresh-contents)
 
 (org-babel-load-file
  (expand-file-name "config.org" "~/.emacs.d"))
